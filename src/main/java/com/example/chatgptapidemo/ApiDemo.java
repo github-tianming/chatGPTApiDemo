3202cad21f17432cbf8357b67c8f2da62fc803de
zhang.tianming <zhangtianming@variflight.com>
2023-04-07 00:17:12 +0800

    ApiDemo

--- src/main/java/com/example/chatgptapidemo/ApiDemo.java
+++ src/main/java/com/example/chatgptapidemo/ApiDemo.java
@@ -1,7 +1,24 @@
 package com.example.chatgptapidemo;
 
+import okhttp3.*;
+
 /**
  * @author zhang.tianming
  */
 public class ApiDemo {
+    public static void main(String[] args) throws Exception {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"prompt\": \"Hello, how are you?\", \"temperature\": 0.5, \"max_tokens\": 50}");
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/engines/davinci-codex/completions")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer 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")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
 }
