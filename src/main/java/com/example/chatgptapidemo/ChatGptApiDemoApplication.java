f9a1b623cd140c90eec19ffbdf64f7f1317746a2
zhang.tianming <zhangtianming@variflight.com>
2023-04-06 23:45:44 +0800

    Initial commit

---
+++ src/main/java/com/example/chatgptapidemo/ChatGptApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGptApiDemoApplication.class, args);
+    }
+
+}
